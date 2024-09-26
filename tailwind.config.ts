20830db07eb2caca74d985d0076958eefcac86f6
Matt-Cesco <matteo.francesconi88@gmail.com>
2024-09-27 00:16:44 +0100

    prettier config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,15 +1,14 @@
-import type { Config } from "tailwindcss";
+/** @type {import("tailwindcss").Config} */
 
-const config: Config = {
-    content: ["./src/pages/**/*.{js,ts,jsx,tsx,mdx}", "./src/components/**/*.{js,ts,jsx,tsx,mdx}", "./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
-    theme: {
-        extend: {
-            colors: {
-                background: "var(--background)",
-                foreground: "var(--foreground)",
-            },
-        },
-    },
-    plugins: [],
+module.exports = {
+	content: ['./src/pages/**/*.{js,ts,jsx,tsx,mdx}', './src/components/**/*.{js,ts,jsx,tsx,mdx}', './src/app/**/*.{js,ts,jsx,tsx,mdx}'],
+	theme: {
+		extend: {
+			colors: {
+				background: 'var(--background)',
+				foreground: 'var(--foreground)',
+			},
+		},
+	},
+	plugins: ['prettier-plugin-tailwindcss'],
 };
-export default config;
