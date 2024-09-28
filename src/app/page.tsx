c8d63552c41f9fe77fab160f125b183b2a722eb3
Matt-Cesco <matteo.francesconi88@gmail.com>
2024-09-28 16:07:52 +0100

    should work now

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,17 @@
-import Image from 'next/image';
-
 export default function Home() {
 	return (
 		<section>
-			<div className='px-8-90 bg-peach'>
+			<div className='bg-peach px-8-90'>
 				<div className='grid grid-cols-6 grid-rows-5 gap-4'>
-					<div className='text-20-180 leading-90 col-span-6 font-extrabold uppercase'>Creating</div>
-					<div className='text-20-180 leading-90 col-span-6 row-start-2 text-end font-extrabold uppercase'>Powerful</div>
-					<div className='text-20-180 leading-90 col-span-6 row-start-3 font-extrabold uppercase'>Experiences</div>
-					<div className='text-18 mt-75 col-span-2 col-start-4 row-span-2 row-start-4'>
+					<div className='col-span-6 text-20-180 font-extrabold uppercase leading-90'>Creating</div>
+					<div className='col-span-6 row-start-2 text-end text-20-180 font-extrabold uppercase leading-90'>Powerful</div>
+					<div className='col-span-6 row-start-3 text-20-180 font-extrabold uppercase leading-90'>Experiences</div>
+					<div className='col-span-2 col-start-4 row-span-2 row-start-4 mt-75 text-18'>
 						Were a creative web design and branding agency based in Birmingham that crafts inpactfull web experience grown brands
 					</div>
+					<p className='mb-4 ml-2 mt-2 px-3 pt-2'>test</p>
 				</div>
 			</div>
-			
 		</section>
 	);
 }
