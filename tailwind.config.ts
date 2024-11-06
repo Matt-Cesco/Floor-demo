7ecfbe3d79e2e89a1792e109ab8f06acb1c146a6
Matt-Cesco <matteo.francesconi88@gmail.com>
2024-11-06 14:53:32 +0000

    tailwind config fixed

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,10 +15,20 @@ interface LineHeightMap {
 	[key: string]: string;
 }
 
-const generateGridColumns = (maxColumns) => {
-	const gridColumnsSpan = {};
-	const gridColumnsStart = {};
-	const gridColumnsEnd = {};
+interface GridColumnMap {
+	[key: string]: string;
+}
+
+const generateGridColumns = (
+	maxColumns: number
+): {
+	gridColumnsSpan: GridColumnMap;
+	gridColumnsStart: GridColumnMap;
+	gridColumnsEnd: GridColumnMap;
+} => {
+	const gridColumnsSpan: GridColumnMap = {};
+	const gridColumnsStart: GridColumnMap = {};
+	const gridColumnsEnd: GridColumnMap = {};
 
 	// Generate `col-span-X` classes (for example: col-span-1, col-span-2, ..., col-span-36)
 	for (let i = 1; i <= maxColumns; i++) {
@@ -27,12 +37,12 @@ const generateGridColumns = (maxColumns) => {
 
 	// Generate `col-start-X` classes
 	for (let i = 1; i <= maxColumns; i++) {
-		gridColumnsStart[i] = `${i}`; // Generates col-start-X classes
+		gridColumnsStart[i.toString()] = `${i}`; // Generates col-start-X classes
 	}
 
 	// Generate `col-end-X` classes
 	for (let i = 1; i <= maxColumns; i++) {
-		gridColumnsEnd[i] = `${i}`; // Generates col-end-X classes
+		gridColumnsEnd[i.toString()] = `${i}`; // Generates col-end-X classes
 	}
 
 	// Add auto classes
