ae4a7ed4a767b333247c58c8c095c419eadc87f5
Matt-Cesco <matteo.francesconi88@gmail.com>
2024-11-07 15:00:23 +0000

    working version

--- src/Components/FlexibleBlocks/ImageTextBlock/ImageTextBlock.tsx
+++ src/Components/FlexibleBlocks/ImageTextBlock/ImageTextBlock.tsx
@@ -1,13 +1,23 @@
-import IImageTextBlock from './IImageTextBlock'; // Updated import
+'use client'
+
+import { useEffect, useRef } from 'react';
+import IImageTextBlock from './IImageTextBlock';
 import IFlexibleBlock from '../IFlexibleBlock';
 import { VariationsOptions } from './ImageTextBlockOptionsEnum';
 import DynamicImage from '@/Common/DynamicImage/DynamicImage';
 import DynamicText from '@/Common/DynamicText/DynamicText';
+import { gsapImageTextBlock } from './gsapImageTextBlock';
 
 const ImageTextBlock = ({ data }: IFlexibleBlock<IImageTextBlock>) => {
 	const { textFirst, textSecond, bigImage, layoutOptions, smallImage } = data.imageTextFields || {};
-
 	const { variationsOptions } = layoutOptions || {};
+
+	const bigImageRef = useRef(null);
+	const smallImageRef = useRef(null);
+
+	useEffect(() => {
+		gsapImageTextBlock(bigImageRef, smallImageRef);
+	}, []);
 
 	return (
 		<section className='my-162'>
@@ -17,7 +27,7 @@ const ImageTextBlock = ({ data }: IFlexibleBlock<IImageTextBlock>) => {
 						<DynamicText data={textFirst} className='my-88 text-22 text-gray lg:my-0' />
 					</div>
 					<div className='col-span-10 col-start-2 lg:col-span-16 lg:col-start-19'>
-						<DynamicImage data={bigImage} className='aspect-[700/400] h-full w-full object-cover' />
+						<DynamicImage ref={bigImageRef} data={bigImage} className='aspect-[700/400]' />
 					</div>
 				</div>
 			)}
@@ -25,7 +35,7 @@ const ImageTextBlock = ({ data }: IFlexibleBlock<IImageTextBlock>) => {
 				<>
 					<div className='grid grid-cols-12 gap-0 lg:grid-cols-36'>
 						<div className='order-2 col-span-10 col-start-2 lg:order-1 lg:col-span-12 lg:col-start-3'>
-							<DynamicImage data={bigImage} className='aspect-[530/690] h-full w-full object-cover' />
+							<DynamicImage ref={bigImageRef} data={bigImage} className='aspect-[530/690]' />
 						</div>
 						<div className='order-1 col-span-10 col-start-2 lg:order-2 lg:col-span-12 lg:col-start-17'>
 							<DynamicText data={textFirst} className='mb-88 text-22 text-gray lg:mb-0' />
@@ -36,7 +46,7 @@ const ImageTextBlock = ({ data }: IFlexibleBlock<IImageTextBlock>) => {
 							<DynamicText data={textSecond} className='my-88 text-22 text-gray' />
 						</div>
 						<div className='col-span-10 col-start-2 mb-162 lg:absolute lg:-top-[100%] lg:col-span-16 lg:col-start-19'>
-							<DynamicImage data={bigImage} className='aspect-[700/400] h-full w-full object-cover' />
+							<DynamicImage ref={smallImageRef} data={smallImage} className='aspect-[700/400]' />
 						</div>
 					</div>
 				</>
@@ -44,7 +54,7 @@ const ImageTextBlock = ({ data }: IFlexibleBlock<IImageTextBlock>) => {
 			{variationsOptions === VariationsOptions.VARIATION3 && (
 				<div className='grid grid-cols-12 gap-0 lg:grid-cols-36'>
 					<div className='order-2 col-span-10 col-start-2 lg:order-1 lg:col-span-12 lg:col-start-3'>
-						<DynamicImage data={bigImage} className='aspect-[530/690] h-full w-full object-cover' />
+						<DynamicImage ref={bigImageRef} data={bigImage} className='aspect-[530/690]' />
 					</div>
 					<div className='order-1 col-span-10 col-start-2 mb-88 flex flex-col justify-between lg:order-2 lg:col-span-10 lg:col-start-17 lg:mb-0'>
 						<DynamicText data={textFirst} className='mb-44 text-22 text-gray lg:mb-0' />
--- src/Components/FlexibleBlocks/ServicesRowsBlock/ServicesRowsBlock.tsx
+++ src/Components/FlexibleBlocks/ServicesRowsBlock/ServicesRowsBlock.tsx
@@ -1,8 +1,10 @@
-import IServicesRowsBlock from './IServicesRowsBlock'; // Updated import
+import IServicesRowsBlock from './IServicesRowsBlock';
 import IFlexibleBlock from '../IFlexibleBlock';
 import DynamicHeading from '@/Common/DynamicHeading/DynamicHeading';
 import Link from 'next/link';
 import DynamicText from '@/Common/DynamicText/DynamicText';
+import Line from '@/Components/Line/Line';
+import DynamicImage from '@/Common/DynamicImage/DynamicImage';
 
 const ServicesRowsBlock = ({ data }: IFlexibleBlock<IServicesRowsBlock>) => {
 	const { sideLeftText, text, heading, servicesRowsCards } = data.servicesRowsFields || {};
@@ -24,27 +26,31 @@ const ServicesRowsBlock = ({ data }: IFlexibleBlock<IServicesRowsBlock>) => {
 			</div>
 			<div className='my-142'>
 				<div className='grid grid-cols-12 gap-0 lg:grid-cols-36'>
-					<span className='col-start-2 col-end-12 border-t border-gray lg:col-start-3 lg:col-end-35'></span>
+					<Line />
 				</div>
-				<ul className='border-b'>
-					{servicesRowsCards?.map((card, i) => (
-						<li key={i}>
-							<Link href={card?.projectPageLink?.url || '#'} target={card?.projectPageLink?.target || '_self'}>
-								<div className='grid grid-cols-12 gap-0 lg:grid-cols-36'>
-									<div className='col-span-1 col-start-2 mt-22 lg:col-start-3'>
-										<p className='text-16 text-yellow'>{card?.serviceNumber}</p>
-									</div>
-									<div className='col-span-10 col-start-2 mb-42 mt-22 lg:col-span-16 lg:col-start-7 lg:my-44-80'>
-										<p className='text-30-75 leading-103 tracking-tight text-gray'>{card?.serviceTitle}</p>
-									</div>
-								</div>
-								<div className='grid grid-cols-12 gap-0 lg:grid-cols-36'>
-									<span className='col-start-2 col-end-12 border-t border-gray lg:col-start-3 lg:col-end-35'></span>
-								</div>
-							</Link>
-						</li>
-					))}
-				</ul>
+				<ul className=''>
+			{servicesRowsCards?.map((card, i) => (
+				<li key={i} className='relative group'>
+					<Link href={card?.projectPageLink?.url || '#'} target={card?.projectPageLink?.target || '_self'}>
+						<div className='grid grid-cols-12 gap-0 lg:grid-cols-36'>
+							<div className='col-span-1 col-start-2 mt-22 lg:col-start-3'>
+								<p className='text-16 text-yellow'>{card?.serviceNumber}</p>
+							</div>
+							<div className='col-span-10 col-start-2 mb-42 mt-22 lg:col-span-16 lg:col-start-7 lg:my-44-80'>
+								<p className='text-30-75 leading-103 tracking-tight text-gray'>{card?.serviceTitle}</p>
+							</div>
+							<DynamicImage
+								data={card?.image}
+								className='absolute right-1/4 -translate-y-1/4 aspect-[400/400] w-470 z-10 opacity-0 transform transition-all duration-500 group-hover:opacity-100 group-hover:rotate-12'
+							/>
+						</div>
+						<div className='grid grid-cols-12 gap-0 lg:grid-cols-36 relative'>
+							<Line />
+						</div>
+					</Link>
+				</li>
+			))}
+		</ul>
 			</div>
 		</section>
 	);
