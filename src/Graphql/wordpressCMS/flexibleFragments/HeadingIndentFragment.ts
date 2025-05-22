763224dc6aff8c4580a850c2e1181d504e8bbf7f
Matt-Cesco <matteo.francesconi88@gmail.com>
2025-05-22 15:01:12 +0100

    prettier chan

--- src/Graphql/wordpressCMS/flexibleFragments/HeadingIndentFragment.ts
+++ src/Graphql/wordpressCMS/flexibleFragments/HeadingIndentFragment.ts
@@ -1,12 +1,11 @@
-import { gql } from '@apollo/client';
+import { gql } from "@apollo/client";
 
 export const HeadingIndentFragment = gql`
-  fragment HeadingIndentFragment on FlexibleContentFlexibleContentBlockHeadingIndentLayout {
-    __typename
-    headingIndentFields {
-        columnIndentation
-      content
-      }
-  }
+    fragment HeadingIndentFragment on FlexibleContentFlexibleContentBlockHeadingIndentLayout {
+        __typename
+        headingIndentFields {
+            columnIndentation
+            content
+        }
+    }
 `;
-  
